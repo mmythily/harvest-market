5610c29f9206cd4a65ef565b741f5146c164a122
mmythily <mythily.mudunuru@gmail.com>
2020-03-05 21:58:39 -0500

    modify sign in form

--- src/pages/sign-and-register/SignRegister.js
+++ src/pages/sign-and-register/SignRegister.js
@@ -1,57 +1,12 @@
 import React from 'react'
 import Nav from '../../components/navigation/Nav'
+import SignIn from '../../components/sign-in/SignIn'
 
-export default class SignRegister extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    this.setState({ email: '', password: '' });
-  };
-
-  handleChange = event => {
-    const { value, name } = event.target;
-
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    return (
-      <>
-      <Nav />
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={this.handleSubmit} >
-          <input
-            name='email'
-            type='email'
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label='email'
-            required
-          />
-          <input
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='password'
-            required
-          />
-          <button type='submit'> Sign in </button>
-        </form>
-      </div>
-      </>
-    );
-  }
+export default function SignRegister() {
+  return (
+    <div>
+      <Nav/>
+      <SignIn />
+    </div>
+  )
 }
